cb8b5c0a9db0052fddefe8b6eb0603d60b27c5ff
SeregaNag <Serega_nag@tut.by>
2024-12-14 23:28:42 +0300

    Add not found page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,11 @@
 import FavoritesPage from 'pages/FavoritesPage/FavoritesPage';
 import HomePage from 'pages/HomePage/HomePage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import PaintingDetailPage from 'pages/PaintingDetailPage/PaintingDetailPage';
 
 export const routes = [
   { path: '/', component: HomePage },
   { path: '/artwork/:id', component: PaintingDetailPage },
   { path: '/favorites', component: FavoritesPage },
+  { path: '*', component: NotFoundPage },
 ];
